9dd95ac72c45543720a69177831b1bb5446d78c7
zzzcta <zzzctadev@gmail.com>
2025-01-23 11:01:06 +0100

    AudioSlip

--- Assets/_Scripts/Trampas/Slip.cs
+++ Assets/_Scripts/Trampas/Slip.cs
@@ -16,15 +16,22 @@ public class Slip : MonoBehaviour
             // Activar la animación
             playerAnimator.SetBool("isSlip", true);
             FindAnyObjectByType<AudioManager>().Play("Slip");
-
+            AwaitAndFallSound();
             // Iniciar la tarea asincrónica
-            AwaitAndExecute();
+            AwaitAndDead();
         }
     }
 
-    private async void AwaitAndExecute()
+    private async void AwaitAndDead()
     {
         await Task.Delay(TimeSpan.FromSeconds(0.8));
         gE.Dead();
     }
+
+    private async void AwaitAndFallSound()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(0.5));
+        FindAnyObjectByType<AudioManager>().Play("BodyFall");
+    }
+
 }
